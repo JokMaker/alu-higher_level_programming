fd3ba5832cbdd5459e3047d8d6b3f12200422dc2
JokMaker <j.kur@alustudent.com>
2024-07-22 06:33:59 -0700

    done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""A script that
-- fetches http://0.0.0.0:5050/status.
-- uses urlib package
+"""A script that:
+- fetches http://localhost:5050/status.
+- uses urllib package
 """
 
+import urllib.request
 
 if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('http://0.0.0.0:5050/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    try:
+        with urllib.request.urlopen('http://localhost:5050/status') as res:
+            content = res.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    except urllib.error.URLError as e:
+        print(f"URL Error: {e.reason}")
+    except Exception as e:
+        print(f"Error: {e}")
