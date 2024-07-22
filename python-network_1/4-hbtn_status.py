7ee4bc90f10786f8965e747958510658fc3583ba
JokMaker <j.kur@alustudent.com>
2024-07-21 17:40:13 -0700

    done

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Documented now"""
 import requests
 
 if __name__ == '__main__':
-    response = requests.get("https://intranet.hbtn.io/status")
+    url = 'https://alu-intranet.hbtn.io/status'
+    response = requests.get(url)
+    content = response.text
+
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
