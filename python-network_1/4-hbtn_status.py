d79a9ee4e53c1ada5fec0edcbf697d934acb8a85
JokMaker <j.kur@alustudent.com>
2024-07-22 07:03:27 -0700

    done

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""Fetches and displays the status of https://alu-intranet.hbtn.io/status."""
-
+"""__summary__
+- Write a Python script that
+- fetches https://intranet.hbtn.io/status.
+"""
 import requests
 
-def fetch_status(url):
-    """Fetches and prints the status of a given URL."""
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
-        print("Body response:")
-        print("\t- type: {}".format(type(response.text)))
-        print("\t- content: {}".format(response.text))
-    except requests.RequestException as e:
-        print(f"Request Error: {e}")
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    fetch_status(url)
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
