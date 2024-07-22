50419f513a7cda7c310ca5871bcc68d92f06fcfa
JokMaker <j.kur@alustudent.com>
2024-07-22 06:39:11 -0700

    done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""A script that:
-- fetches http://localhost:5050/status.
-- uses urllib package
+"""__summary__
+- Write a Python script that fetches https://alu-intranet.hbtn.io/status
+- using the urllib package.
 """
-
 import urllib.request
 
+
 if __name__ == '__main__':
-    try:
-        with urllib.request.urlopen('http://localhost:5050/status') as res:
-            content = res.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(content)))
-            print("\t- content: {}".format(content))
-            print("\t- utf8 content: {}".format(content.decode('utf-8')))
-    except urllib.error.URLError as e:
-        print(f"URL Error: {e.reason}")
-    except Exception as e:
-        print(f"Error: {e}")
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
