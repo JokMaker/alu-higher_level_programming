b11e443a28bec41f6bdc7d1e017d6c169f6a52b5
JokMaker <j.kur@alustudent.com>
2024-07-22 07:17:48 -0700

    done

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-
-import numpy as np\
-
 """Defines a matrix multiplication function using NumPy."""
-'''
-File_name:
-Created: 5-DEC-2023
-Author: UMUTONI Kevine (simplykevine)
-Size: Large
-Project: python-test_driven_development
-Status: Not yet submted
-'''
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-
     """Return the multiplication of two matrices.
 
     Args:
